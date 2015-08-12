6a77c2f25d2eab10784a1d7adb08a6ff68c97047
Richard Stelling <richard@empiricalmagic.com>
2015-08-12 19:55:21 +0100

    Stub for convenience category.

---
+++ Pod/Classes/NSDate+SimpleDateFormatter.h
@@ -0,0 +1,13 @@
+//
+//  NSDate+SimpleDateFormatter.h
+//  SimpleDateFormatter
+//
+//  Created by Richard Stelling on 12/08/2015.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (SimpleDateFormatter)
+
+@end
---
+++ Pod/Classes/NSDate+SimpleDateFormatter.m
@@ -0,0 +1,14 @@
+//
+//  NSDate+SimpleDateFormatter.m
+//  SimpleDateFormatter
+//
+//  Created by Richard Stelling on 12/08/2015.
+//
+//
+
+#import "NSDate+SimpleDateFormatter.h"
+#import "SDFSimpleDateFormatter.h"
+
+@implementation NSDate (SimpleDateFormatter)
+
+@end
